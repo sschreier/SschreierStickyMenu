566be306e9a6b7969facd76f4744282c2524ac32
Sebastian Schreier <info@sebastianschreier.de>
2023-05-01 18:59:24 +0200

    refactored code to convert plugin config values to SCSS variables

--- src/SschreierStickyMenu.php
+++ src/SschreierStickyMenu.php
@@ -1,28 +1,15 @@
-<?php 
+<?php
 declare(strict_types=1);
 
 namespace Sschreier\StickyMenu;
 
 use Shopware\Core\Framework\Plugin;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class SschreierStickyMenu extends Plugin 
+class SschreierStickyMenu extends Plugin
 {
-	/* plugin variables match: config key => scss variable */
-    const PLUGIN_VARS = [
-        'backgroundColorStickyMenu' => 'sschreier-stickymenu-backgroundcolor',
-        'zIndexStickyMenu' => 'sschreier-stickymenu-zindex',
-        'transitionDurationStickyMenu' => 'sschreier-stickymenu-transition-duration',
-        'boxShadowStickyMenu' => 'sschreier-stickymenu-boxshadow',
+    const PLUGIN_CONFIG_VARS = [
+        'zIndexStickyMenu',
+        'transitionDurationStickyMenu',
+        'boxShadowStickyMenu'
     ];
-
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-
-        $container->setParameter(
-            'sschreier_stickymenu_domain',
-            $this->getName() . '.config.'
-        );
-    }
 }
